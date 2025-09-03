fa7e2a78fa033f673f7ae4ff95fd0a4dd1a14a78
Kirill Knyazev <rizorpolder@gmail.com>
2025-09-03 19:42:09 +0300

    min

--- Assets/Scripts/UI/Clicker/UI/UIController.cs
+++ Assets/Scripts/UI/Clicker/UI/UIController.cs
@@ -1,4 +1,5 @@
 using System;
+using DG.Tweening;
 using Managers.Clicker;
 using UI.Clicker.UI.HUD;
 using UnityEngine;
@@ -12,11 +13,13 @@ namespace UI.Clicker.UI
 		[SerializeField] private AResourcePanel _energyPanel;
 		[SerializeField] private AResourcePanel _currencyPanel;
 		[SerializeField] private Button _collectButton;
+		[SerializeField] private ParticleSystem _particles;
 
 		[Inject] private IClickerListener _clickerListener;
 		[Inject] private IClickerCommand _clickerCommand;
 		[Inject] private IClickerData _clickerData;
 
+		private Sequence _tween;
 		private void Start()
 		{
 			_collectButton.onClick.AddListener(OnButtonClickHandler);
@@ -42,15 +45,33 @@ namespace UI.Clicker.UI
 			_energyPanel.SetResourceText(_clickerData.UserEnergy);
 		}
 
+
+		// beware of magic numbers (:
 		private void OnCurrencyDataChanged()
 		{
 			_currencyPanel.SetResourceText(_clickerData.UserCurrency);
-			//TODO Create VFX and DoTween item;
+			_particles.Emit(10);
+			PlayButtonAnimation();
+			//Emit from pool currency
+		}
+
+
+		// beware of magic numbers (:
+		private void PlayButtonAnimation()
+		{
+			_tween?.Kill();
+			_tween = DOTween.Sequence();
+			_tween.Append(_collectButton.transform.DOScale(1.1f, 0.1f));
+			_tween.Join(_collectButton.image.DOColor(new Color(0.5f, 1f, 0.5f, 1f), 0.1f));
+			_tween.Append(_collectButton.transform.DOScale(1.0f, 0.1f));
+			_tween.Join(_collectButton.image.DOColor(Color.white, 0.1f));
+			_tween.Play();
 		}
 
 		private void OnButtonClickHandler()
 		{
 			_clickerCommand.AddCurrency();
+
 		}
 	}
 }
